0e1ff46f9de9922b503920ba188b77ad38ba6bd3
Kevin-879 <ramirez105.kr@gmail.com>
2024-04-10 21:02:29 -0500

    prueba de graficos

---
+++ prueba/src/app.js
@@ -0,0 +1,120 @@
+import Chart from 'chart.js/auto'
+import { getAquisitionsByYear } from './api'
+
+(async function() {
+  const data = await getAquisitionsByYear();
+  new Chart(
+    document.getElementById('grafico'),
+    {
+      type: 'bar',
+      options: {
+        animation: false,
+        plugins: {
+          legend: {
+            display: false
+          },
+          tooltip: {
+            enabled: false
+          }
+        }
+      },
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import Chart from 'chart.js/auto';
+
+
+const graficos = document.getElementById('grafico');
+  
+    new Chart(graficos, {
+      type: 'bar',
+      data: {
+        labels: ['temperatura'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+
+
+function newGrafico() {
+  const data = [
+    { year: 2010, count: 10 },
+    { year: 2011, count: 20 },
+    { year: 2012, count: 15 },
+    { year: 2013, count: 25 },
+    { year: 2014, count: 22 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 28 },
+  ];
+
+  new Chart(
+    document.getElementById('grafico'),
+    {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+}
+
+newGrafico();*/
