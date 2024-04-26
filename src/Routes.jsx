09dac44e412ecc05249787cf31bbf73b95f61829
jeissonR115 <jeissonfeliper@gmail.com>
2024-04-25 23:33:48 -0500

    login routmap

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,22 +1,32 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import App from './App';
-import Calendar from './calendar/index'
-import Login from './login/'
+import Calendar from './calendar/index';
+import Login from './login/';
 
 const AppRoutes = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado para controlar si el usuario está autenticado
+
+    // Componente de ruta privada
+    const PrivateRoute = ({ element, ...rest }) => {
+        return isLoggedIn ? element : <Navigate to="/login" />;
+    };
+
     return (
         <BrowserRouter basename='/ambidata_PIA__frontend/'>
             <Routes>
-                <Route path="/App" element={<App />} />
-                <Route path="/calendar" element={<Calendar />} />
-                <Route path="/" element={<Login />} />
+                {/* Rutas públicas */}
+                <Route path="/login" element={<Login setIsLoggedIn={setIsLoggedIn} />} />
+
+                {/* Rutas protegidas */}
+                <Route path="/App/*" element={<PrivateRoute element={<App />} />} />
+                <Route path="/calendar" element={<PrivateRoute element={<Calendar />} />} />
+                
+                {/* Redireccionamiento por defecto */}
+                <Route path="/*" element={<Navigate to="/login" />} />
             </Routes>
         </BrowserRouter>
     );
 };
 
 export default AppRoutes;
-
-
-
--- src/login/index.jsx
+++ src/login/index.jsx
@@ -1,81 +1,68 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import './styles.css';
-import arbolitoSVG from '/arbolito.svg';
-import Redirect from 'react-dom/client'
-class Login extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            username: '',
-            password: '',
-            error: ''
-        };
+import { useNavigate } from 'react-router-dom';
+
+const Login = ({ setIsLoggedIn }) => { // Recibe setIsLoggedIn como una prop
+    const navigate = useNavigate();
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
     }
 
-    handleUsernameChange = (e) => {
-        this.setState({ username: e.target.value });
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
     }
 
-    handlePasswordChange = (e) => {
-        this.setState({ password: e.target.value });
-    }
-
-    handleLogin = async () => {
-        const { username, password } = this.state;
+    const handleLogin = async () => {
         try {
             const response = await axios.post('http://localhost:3000/login', {
                 username: username,
                 password: password
             });
-            this.setState({ error: null, redirectToRoot: true });
-            console.log(response.data); // Manejar la respuesta del servidor según sea necesario
+            setError(null);
+            setIsLoggedIn(true); // Marcar al usuario como autenticado
+            navigate('/App');
         } catch (error) {
             if (error.response && error.response.status === 401) {
-                this.setState({ error: 'Credenciales inválidas' });
-            }
-            else {
+                setError('Credenciales inválidas');
+            } else {
                 console.error('Error en la solicitud de inicio de sesión:', error);
             }
         }
     };
 
-    render() {
-        const { username, password, error, redirectToRoot } = this.state;
-        if (redirectToRoot) {
-            return <Redirect to="/ambidata_PIA__frontend" />;
-        }
-        return (
-            <div className='fondo'>
-                {/*<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"></link>*/}
-                <div className='login-container'>
-                    <div className="login-box">
-                        <div className="flex-container">
-                            <div className="img-container">
-                                {/* <img src={arbolitoSVG} alt="Árbol Genealógico" className="arbolito-svg" /> */}
-                                <img className='imagen' src="/ambidata_PIA__frontend/public/6229893x2.jpg"></img>
-                            </div>
-                            <div className="form-container">
-                                <h1 className='form-title'>User Login</h1>
-                                <form className='form'>
-                                    <div className="input-group">
-                                        <input type="text" placeholder="Username" value={username} onChange={this.handleUsernameChange} />
-                                        <span className="input-icon"><i className="fas fa-user"></i></span>
-                                    </div>
-                                    <div className="input-group">
-                                        <input type="password" placeholder="Password" value={password} onChange={this.handlePasswordChange} />
-                                        <span className="input-icon"><i className="fas fa-lock"></i></span>
-                                    </div>
-                                    {error && <div className="error-message">{error}</div>}
-                                    <button className='form-button' type="button" onClick={this.handleLogin}>Login</button>
-                                </form>
-                            </div>
+    return (
+        <div className='fondo'>
+            <div className='login-container'>
+                <div className="login-box">
+                    <div className="flex-container">
+                        <div className="img-container">
+                            <img className='imagen' src="/ambidata_PIA__frontend/public/6229893x2.jpg" alt="Login" />
+                        </div>
+                        <div className="form-container">
+                            <h1 className='form-title'>User Login</h1>
+                            <form className='form'>
+                                <div className="input-group">
+                                    <input type="text" placeholder="Username" value={username} onChange={handleUsernameChange} />
+                                    <span className="input-icon"><i className="fas fa-user"></i></span>
+                                </div>
+                                <div className="input-group">
+                                    <input type="password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+                                    <span className="input-icon"><i className="fas fa-lock"></i></span>
+                                </div>
+                                {error && <div className="error-message">{error}</div>}
+                                <button className='form-button' type="button" onClick={handleLogin}>Login</button>
+                            </form>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Login;
