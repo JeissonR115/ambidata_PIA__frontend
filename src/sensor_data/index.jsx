ae1f90c2d549c8ac942cf13eee94e2bd01d6ce78
jeissonR115 <jeissonfeliper@gmail.com>
2024-04-26 22:28:51 -0500

    paginacion

--- src/sensor_data/index.jsx
+++ src/sensor_data/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.css';
 import { colorear } from './color.js';
 import Graph from '../graph';
@@ -7,43 +7,78 @@ import DownloadSensorData from '../downloadSensorData';
 const styleElement = document.createElement('style');
 document.head.appendChild(styleElement);
 const styleSheet = styleElement.sheet; // Hoja de estilo CSS
+
 // Esta función representa los datos del sensor
 function SensorData({ data, searchValue }) {
-  // Si no hay datos, mostrar el texto "empezar a buscar"
   if (data === null) {
     return <p>Buscando...</p>;
   }
+  const itemsPerPage = 5; // Número de elementos por página
+  const [currentPage, setCurrentPage] = useState(1);
+  const [showAllData, setShowAllData] = useState(false); // Estado para controlar si se muestran todos los datos o solo los de la página actual
+
+  // Calcula el índice inicial y final de los elementos para la página actual
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = data.slice(indexOfFirstItem, indexOfLastItem);
+
   // Si no ha Empezado a buscar, mostrar el texto "empezar a buscar"
   if (searchValue.length === 0) {
     return <p>Empezar a buscar...</p>;
   }
   // Si no encontró datos, mostrar el texto "no se encontró"
-  if (data.length === 0) {
+  if (currentItems.length === 0) {
     return <p>No se encontraron datos con la búsqueda de : {searchValue}</p>;
   }
-  colorear(styleSheet, { value: 120, saturation: 50, lightness: 90 }, data.length);
-  // Si hay datos, mostrar los elementos del array
+
+  // Colorear según el número de datos
+  colorear(styleSheet, { value: 120, saturation: 50, lightness: 90 }, currentItems.length);
+
+  // Función para cambiar de página
+  const paginate = pageNumber => setCurrentPage(pageNumber);
+
   return (
     <>
       <div className='sensor-data'>
-        {data.map((sensor, index) => (
-          // Renders each sensor
-          <div key={index} className={`sensor sensor--${index}`}>
-            <p>
-              humedad: {sensor.ambient}% - temperatura: {sensor.temperature}°C - fecha: {sensor.fecha.match(/^(\d{4}-\d{2}-\d{2})/)[1]} hora: {sensor.fecha.match(/T(\d{2}:\d{2}:\d{2})/)[1]}
-            </p>
-          </div>
-        ))}
+        {showAllData ? (
+          // Mostrar todos los datos
+          data.map((sensor, index) => (
+            <div key={index} className={`sensor sensor--${index}`}>
+              <p>
+                humedad: {sensor.ambient}% - temperatura: {sensor.temperature}°C - fecha: {sensor.fecha.match(/^(\d{4}-\d{2}-\d{2})/)[1]} hora: {sensor.fecha.match(/T(\d{2}:\d{2}:\d{2})/)[1]}
+              </p>
+            </div>
+          ))
+        ) : (
+          // Mostrar solo los datos de la página actual
+          currentItems.map((sensor, index) => (
+            <div key={index} className={`sensor sensor--${index}`}>
+              <p>
+                humedad: {sensor.ambient}% - temperatura: {sensor.temperature}°C - fecha: {sensor.fecha.match(/^(\d{4}-\d{2}-\d{2})/)[1]} hora: {sensor.fecha.match(/T(\d{2}:\d{2}:\d{2})/)[1]}
+              </p>
+            </div>
+          ))
+        )}
+      </div>
+      <div className='pagination'>
+        {/* Botones para cambiar de página */}
+        <button onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1}>Anterior</button>
+        <span>Página {currentPage}</span>
+        <button onClick={() => paginate(currentPage + 1)} disabled={indexOfLastItem >= data.length}>Siguiente</button>
       </div>
       <div className='sensor-controls'>
         <div className='sensor-graph'>
-          <Graph data={data} />
+          {/* Botón para cambiar entre mostrar todos los datos y solo los de la página actual */}
+          <button className='all-data' onClick={() => setShowAllData(!showAllData)}>
+            {showAllData ? 'Mostrar Paginados' : 'Mostrar Todos'}
+          </button>
+          {/* Renderizar el gráfico con los datos correspondientes */}
+          <Graph data={showAllData ? data : currentItems} />
         </div>
         <div className='sensor-file'>
-          <DownloadSensorData data={data}></DownloadSensorData>
+          <DownloadSensorData data={currentItems}></DownloadSensorData>
         </div>
       </div>
-
     </>
   );
 }
